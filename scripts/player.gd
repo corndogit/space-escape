351a43710a53a6f078bc31f49cd7cfeb2845e8cc
Nathan <101812777+corndogit@users.noreply.github.com>
2023-07-06 13:32:41 +0100

    change player sprite and other minor tweaks

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,29 +7,42 @@ extends CharacterBody2D
 
 func _process(delta):
 	_calculate_movement(speed * delta)
+	_check_for_interaction()
+
+func _calculate_movement(diff : float):
+	if Input.is_key_pressed(KEY_A):
+		position.x -= diff
+		sight.rotation_degrees = 90
+		sprite.play("walk")
+		if not sprite.flip_h:
+			sprite.flip_h = true
+
+	if Input.is_key_pressed(KEY_D):
+		position.x += diff
+		sight.rotation_degrees = -90
+		sprite.play("walk")
+		if sprite.flip_h:
+			sprite.flip_h = false
+
+	if Input.is_key_pressed(KEY_S):
+		position.y += diff
+		sight.rotation_degrees = 0
+		sprite.play("walk")
+
+	if Input.is_key_pressed(KEY_W):
+		position.y -= diff
+		sight.rotation_degrees = 180
+		sprite.play("walk")
+
+	if not Input.is_anything_pressed():
+		sprite.play("idle")
+	
+	move_and_slide()
+
+func _check_for_interaction():
 	if Input.is_key_pressed(KEY_E):
 		if sight.is_colliding():  # see if there is anything to interact with
 			var target = sight.get_collider()
 			target.get_groups()
 			if target.is_in_group("Interactable"):
 				target.interact()
-	
-func _calculate_movement(diff : float):
-	if Input.is_key_pressed(KEY_A):
-		position.x -= diff
-		sight.rotation_degrees = 90
-		sprite.play("left")
-	if Input.is_key_pressed(KEY_D):
-		position.x += diff
-		sight.rotation_degrees = -90
-		sprite.play("right")
-	if Input.is_key_pressed(KEY_S):
-		position.y += diff
-		sight.rotation_degrees = 0
-		sprite.play("down")
-	if Input.is_key_pressed(KEY_W):
-		position.y -= diff
-		sight.rotation_degrees = 180
-		sprite.play("up")
-	
-	move_and_slide()
