5f1e2e712aec14e2c74c259e34e2a0f85c24023d
Nathan <101812777+corndogit@users.noreply.github.com>
2023-07-11 20:42:13 +0100

    level changes

--- scripts/ComputerTable.gd
+++ scripts/ComputerTable.gd
@@ -1,9 +1,14 @@
 extends Interactable
 
-var powered : bool = false
+var powered : bool = true
+var puzzle_scene = preload("res://scenes/puzzle_2_clue.tscn").instantiate()
+signal open_scene(puzzle_scene)
 
 func _ready():
 	State.computer_table = self
 
 func interact():
-	DialogueManager.show_example_dialogue_balloon(load("res://resources/interactables.dialogue"), "computer_table")
+	if not powered:
+		DialogueManager.show_example_dialogue_balloon(load("res://resources/interactables.dialogue"), "computer_table")
+	else:
+		super.interact()
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,13 +4,16 @@ extends CharacterBody2D
 @export var speed : float = 300.0
 @onready var sprite : AnimatedSprite2D = get_node("AnimatedSprite2D")
 @onready var sight : RayCast2D = get_node("Sight")
+@onready var indicator : Label = get_node("Indicator")
 var in_dialogue : bool = false
 
 func _process(delta):
 	_calculate_movement(speed * delta)
+	_check_for_interactable()
 	
 func _unhandled_input(_event):
-	_check_for_interaction()
+	if Input.is_key_pressed(KEY_E) and _check_for_interactable():
+		_player_interact(sight.get_collider())
 
 func _calculate_movement(diff : float):
 	if in_dialogue:  # disable movement during dialogue
@@ -46,16 +49,18 @@ func _calculate_movement(diff : float):
 	
 	move_and_slide()
 
-func _check_for_interaction():
-	if Input.is_key_pressed(KEY_E):
-		if sight.is_colliding():  # see if there is anything to interact with
-			var target = sight.get_collider()
-			target.get_groups()
-			if target.is_in_group("Interactable"):
-				in_dialogue = true  # entering dialogue
-				target.interact()
-				await DialogueManager.dialogue_ended
-				in_dialogue= false
-			
-				
-
+func _check_for_interactable():
+	if sight.is_colliding():  # see if there is anything to interact with
+		var target = sight.get_collider()
+		target.get_groups()
+		if target.is_in_group("Interactable"):
+			indicator.visible = true
+			return true
+	indicator.visible = false
+	return false
+	
+func _player_interact(target):
+	in_dialogue = true  # entering dialogue
+	target.interact()
+	await DialogueManager.dialogue_ended
+	in_dialogue= false
