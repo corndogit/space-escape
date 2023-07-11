6d406b4a53f44cba6dc796202655f3517f4a6b0f
Nathan <101812777+corndogit@users.noreply.github.com>
2023-07-11 18:12:28 +0100

    add placeholder dialogue to prevent softlock

--- scripts/classes/Interactable.gd
+++ scripts/classes/Interactable.gd
@@ -3,3 +3,5 @@ class_name Interactable
 
 func interact():
 	push_warning("%s has not implemented this function" % name)
+	var dialogue = DialogueManager.create_resource_from_text("~ start \nNot implemented yet.")
+	DialogueManager.show_example_dialogue_balloon(dialogue, "start")
