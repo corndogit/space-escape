421d1d5c672126fadba93828f4bb32dee21c445f
Nathan <101812777+corndogit@users.noreply.github.com>
2023-07-11 20:41:51 +0100

    remove debug print statements

--- scripts/Clue.gd
+++ scripts/Clue.gd
@@ -8,11 +8,9 @@ func _ready():
 	custom_minimum_size.y = window_size.y
 
 func _on_mouse_entered():
-	print("mouse entered %s" % name)
 	closable = true
 	
 func _on_mouse_exited():
-	print("mouse exited %s" % name)
 	closable = false
 	
 func _process(_delta):
