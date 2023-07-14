59695bc2d18fc23ba38c9d7e0980db816d7a3c95
Nathan <admin@corndogit.dev>
2023-07-14 09:40:52 +0100

    add close button to panels

--- scripts/PanelUI.gd
+++ scripts/PanelUI.gd
@@ -7,16 +7,9 @@ func _ready():
 	custom_minimum_size.x = window_size.x
 	custom_minimum_size.y = window_size.y
 
-func _on_mouse_entered():
-	closable = true
-	
-func _on_mouse_exited():
-	closable = false
-	
-func _process(_delta):
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and closable:
-		State.computer_table.close_puzzle()
-
 func _on_puzzle_1_puzzle_solved():
 	await get_tree().create_timer(3).timeout
+	State.computer_table.close_puzzle()
+
+func _on_close_button_pressed():
 	State.computer_table.close_puzzle()
