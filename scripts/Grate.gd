211d2bbd853f59b3c76268ae2df6e5a73591811b
Nathan <101812777+corndogit@users.noreply.github.com>
2023-07-18 18:52:39 +0100

    dialogue changes

--- scripts/Grate.gd
+++ scripts/Grate.gd
@@ -1,7 +1,13 @@
 extends Interactable
 
+var usb_taken : bool = false
+
 func _ready():
 	State.grate = self
 
 func interact():
 	DialogueManager.show_example_dialogue_balloon(load("res://resources/interactables.dialogue"), "grate")
+	
+func take_usb():
+	usb_taken = true
+	self.remove_from_group("Interactable")
