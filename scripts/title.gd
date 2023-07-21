abf86fd5497384656ca2463c7ac8c58cbbd9aba2
Nathan <101812777+corndogit@users.noreply.github.com>
2023-07-21 16:55:44 +0100

    fix background scroll

--- scripts/title.gd
+++ scripts/title.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 @export var scroll_speed : float = 60.0
 @onready var bg = get_node("ScrollBG")
-@onready var bg_midpoint = get_node("ScrollBG/Midpoint")
 @onready var bg_start = bg.position
 @onready var bgm = get_node_or_null("BGM")
+@onready var button_sfx = get_node_or_null("PanelContainer/CenterContainer/VBoxContainer/HowToPlay/Sound")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,10 +19,10 @@ func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/level.tscn")
 
 func _process(delta):
-	if bg_midpoint.position.x < 0:
+	if bg.position.x < -(bg.size.x / 2):
 		bg.position = bg_start
 	bg.position.x -= scroll_speed * delta
 
 func _on_how_to_play_pressed():
-	get_node("PanelContainer/CenterContainer/VBoxContainer/HowToPlay/Sound").play()
+	button_sfx.play()
 	add_child(load("res://scenes/how_to_play.tscn").instantiate())
